161d53d2d74867e2af87883f00de1321128b4605
Даниил Ковалёв <dakovalyov@miem.hse.ru>
2025-08-24 13:44:02 +0200

    update background2

--- progress/scripts.js
+++ progress/scripts.js
@@ -120,3 +120,34 @@ const barTitles = {
     cmc: { RU: "Комиксы", EN: "Comics" },
     tx: { RU: "Текст", EN: "Text" }
 };
+
+document.addEventListener("DOMContentLoaded", () => {
+    const bgImage = new Image();
+    bgImage.src = "../images/CHAOTICVERSE_1.webp";
+
+    bgImage.onload = () => {
+        const aspectRatio = bgImage.height / bgImage.width; // высота / ширина картинки
+        const resizableDiv = document.querySelector("#backgroundHeight");
+
+        function resizeDiv() {
+            const isMobile = /Android|iPhone|iPad|iPod|Windows Phone/i.test(navigator.userAgent);
+            let newWidth, newHeight, backgroundSize;
+
+            if (isMobile) {
+                backgroundSize = "auto 100%";
+                newHeight = window.innerHeight;
+                newWidth = newHeight / aspectRatio;
+            } else {
+                backgroundSize = "100% auto";
+                newWidth = window.innerWidth;
+                newHeight = newWidth * aspectRatio;
+            }
+            resizableDiv.style.backgroundSize = backgroundSize
+            resizableDiv.style.width = `${newWidth}px`;
+            resizableDiv.style.height = `${newHeight}px`;
+        }
+
+        resizeDiv();
+        window.addEventListener("resize", resizeDiv);
+    };
+});
--- scripts.js
+++ scripts.js
@@ -54,3 +54,34 @@ restoreBtn.addEventListener('click', () => {
     overlay.classList.remove('hidden');
     restoreBtn.style.display = 'none';
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const bgImage = new Image();
+    bgImage.src = "images/CHAOTICVERSE_1.webp";
+
+    bgImage.onload = () => {
+        const aspectRatio = bgImage.height / bgImage.width; // высота / ширина картинки
+        const resizableDiv = document.querySelector("#backgroundHeight");
+
+        function resizeDiv() {
+            const isMobile = /Android|iPhone|iPad|iPod|Windows Phone/i.test(navigator.userAgent);
+            let newWidth, newHeight, backgroundSize;
+
+            if (isMobile) {
+                backgroundSize = "auto 100%";
+                newHeight = window.innerHeight;
+                newWidth = newHeight / aspectRatio;
+            } else {
+                backgroundSize = "100% auto";
+                newWidth = window.innerWidth;
+                newHeight = newWidth * aspectRatio;
+            }
+            resizableDiv.style.backgroundSize = backgroundSize
+            resizableDiv.style.width = `${newWidth}px`;
+            resizableDiv.style.height = `${newHeight}px`;
+        }
+
+        resizeDiv();
+        window.addEventListener("resize", resizeDiv);
+    };
+});
